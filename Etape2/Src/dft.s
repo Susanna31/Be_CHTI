0b50d2fe394fa894c95c8e463dd491d1e7e1954c
Susanna Schafer <schafer@insat.insa-toulouse.fr>
2020-05-25 14:48:56 +0200

    ferfeadsz

--- Etape2/Src/dft.s
+++ Etape2/Src/dft.s
@@ -5,20 +5,22 @@
 	import TabSig
 	import somme
 	export re
+	export m2
 ;
 
+;fonction Re(k)
 re proc
 	
 	push{r4, r5, r6, r7}
 	
 	ldr r1, =TabSig
-	ldrsh r1, [r1, r4, lsl #1]
+	ldr r1, [r1, r4, lsl #1]
 	ldr r2, =TabCos
-	ldrsh r2, [r2, r6, lsl #1]
+	ldr r2, [r2, r6, lsl #1]
 	;i -> r4
-	movs r4, #0
+	mov r4, #0
 	;somme -> r5
-	movs r5, #0
+	mov r5, #0
 	
 	;initialisation compteur boucle somme à N-1=63
 	movs r7, #63
@@ -26,25 +28,36 @@ re proc
 	;debut boucle somme
 boucle	
 	;incrementation de i
-	adds r4, #1
+	add r4, #1
 	;ik -> r6
 	mul r6, r4, r0
 	;ik modulo N -> r6
-	ands r6, r6, #63
+	and r6, r6, #63
 	;somme
 	mla r5, r1, r2, r5
 	;decrementation compteur boucle somme
-	subs r7, #1
+	sub r7, #1
 	;retour debut boucle somme
-	cbz r7, boucle
+	;cbnz r7, boucle
+	cmp r7, #0
+	bne boucle
 	;fin boucle somme
 	
 	;valeur de retour dans r0
 	mov r0, r5
 	
-	pop{r4, r5, r6}
+	pop{r4, r5, r6, r7}
 	
 	bx lr
 	endp
+		
+;fonction M2(k)
+m2 proc
+	
+	;appel fonction re
+	bl re
+	
+	
+
 	end
 ;
